d5b5fca37fcc17bb76af70a77f24245c2d2cffbc
dzambranob <dzambranob@users.noreply.github.com>
2023-03-30 21:24:52 +0000

    deploy: 6f4087c0291aef8272072b0e3442b94ecb5b4e09

--- sketches/moirePattern3.js
+++ sketches/moirePattern3.js
@@ -1,47 +1,23 @@
 let angle = 0;
-let spacing = 10;
-
 function setup() {
-  createCanvas(400, 400);
-  noStroke();
-  rectMode(CENTER);
-  //frameRate(30);
+  createCanvas(500, 500);
+  angleMode(DEGREES);
+  strokeWeight(3);
 }
 
 function draw() {
-  background(220);
-  if (keyIsDown(LEFT_ARROW)) {
-    // Draw first set of lines
-    for (let x = -width; x < width; x += spacing) {
-      let y = map(sin(angle + x * 0.01), -1, 1, 0, height);
-      fill(255, 0, 0);
-      rect(x, y, 2, height);
-    }
+  background(255);
+  for (let y = 0; y < height; y += 10) {
+    line(0, y, width, y);
   }
-   else if (keyIsDown(RIGHT_ARROW)) {
-    // Draw second set of lines
-    for (let y = -height; y < height; y += spacing) {
-      let x = map(sin(angle + y * 0.01), -1, 1, 0, width);
-      fill(0, 0, 255);
-      rect(x, y, width, 2);
-    }
+  push();
+  translate(width/2, height/2); 
+  rotate(angle);
+  for (let y = -height; y < height; y += 10) {
+    line(-width/2, y, width/2, y);
   }
- else {
-    // Draw first set of lines
-  for (let x = -width; x < width; x += spacing) {
-    let y = map(sin(angle + x * 0.01), -1, 1, 0, height);
-    fill(255, 0, 0);
-    rect(x, y, 2, height);
-  }
-    // Draw second set of lines
-    for (let y = -height; y < height; y += spacing) {
-      let x = map(sin(angle + y * 0.01), -1, 1, 0, width);
-      fill(0, 0, 255);
-      rect(x, y, width, 2);
-    }
-  }
-  if (!keyIsDown(UP_ARROW)){
-   // Update angle for animation
-    angle += 0.1; 
+  pop();
+  if (!mouseIsPressed){
+    angle += 1
   }
 }
